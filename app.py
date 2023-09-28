83509094663081abb9645853bf48877e90788f23
vaishnavi-kalidindi <vaishnavikalidindi23@gmail.com>
2023-09-28 11:31:00 +0530

    Added user detail functionality

--- app.py
+++ app.py
@@ -15,14 +15,13 @@ password = os.environ.get('SALES_DB_PASS')
 app.config["SQLALCHEMY_DATABASE_URI"] = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 db.init_app(app)
 
-<<<<<<< HEAD
-from models.course import Course
 from models.user import User
-=======
+from models.course import Course
+
  
 from routes import users
 from routes import courses
->>>>>>> 5a1f74cb99875fcfe9491a454660dd6898d879e7
+ 
 
 
 @app.route('/')
@@ -30,11 +29,7 @@ def hello_world():
     rec = db.get_or_404(Course, 1)
     return render_template('index.html', user=rec)
 
-@app.route('/users')
-def users():
-    user_recs = db.session.query(User).all()
-    users = list(map(lambda rec: rec.__dict__, user_recs))
-    return render_template('users.html', users=users)
+
 
  
 
--- routes/users.py
+++ routes/users.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template,request
 from app import app
 from app import db
 from models.user import User
@@ -7,3 +7,10 @@ def users():
     user_recs = db.session.query(User).all()
     users = list(map(lambda rec: rec.__dict__, user_recs))
     return render_template('users.html', users=users)
+
+
+@app.route('/user', methods=['GET'])
+def user():
+    id=request.args.get('id')
+    user=User.query.get(id)
+    return render_template('user_detail.html', user=user)
