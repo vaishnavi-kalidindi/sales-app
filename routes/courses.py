e060cee3874cbb5ae3ee354bf5178df53d94a8b7
vaishnavi-kalidindi <vaishnavikalidindi23@gmail.com>
2023-10-01 11:44:53 +0530

    Added updated logic

--- routes/courses.py
+++ routes/courses.py
@@ -14,16 +14,29 @@ def courses():
 def course():
     course_id = request.args.get('course_id')
     if request.method == 'GET' :
+        course_id = request.args.get('course_id')
         if course_id:
-            course=Course.query.get(course_id)
+            course = Course.query.get(course_id)
+            form_action = request.args.get('action')
+            if form_action == 'edit':
+                return render_template('course_form.html', course=course)
             return render_template('course_detail.html', course=course)
         else:
-            return render_template('course_form.html')
+            return render_template('course_form.html', course=None)
     elif request.method == 'POST':
+        course_id = request.form['course_id']
         course_name = request.form['course_name']
         course_author = request.form['course_author']
         course_endpoint = request.form['course_endpoint']
-        course = Course(
+        if course_id:
+            course = Course.query.get(course_id)
+            course.course_name = course_name,
+            course.course_author = course_author
+            course.course_endpoint = course_endpoint
+            
+
+        else:
+            course = Course(
             course_name=course_name,
             course_author=course_author,
             course_endpoint=course_endpoint
